b7b51eb9e61aadfbb1318e87d2f9782f8b53ae2f
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2024-04-30 01:02:21 +0200

    Insert gateway

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://ju27lkc4c4.execute-api.eu-west-1.amazonaws.com/2401FTDE-REIRAM-CC-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
